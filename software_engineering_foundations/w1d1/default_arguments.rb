ada33c22d0c20243a96bffa868560377dc18d344
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-27 23:15:28 +0000

    default arguments notes added

--- software_engineering_foundations/w1d1/default_arguments.rb
+++ software_engineering_foundations/w1d1/default_arguments.rb
@@ -6,3 +6,20 @@ end
 
 p repeat("hi") # => "hi"
 p repeat("hi", 3) # => "hihihi"
+
+# A fairly common design pattern is to set
+# an arg to nil by default and have logic 
+# based on that scenario
+def greet(person_1, person_2=nil)
+  if person_2.nil?
+    p "Hey " + person_1
+  else
+    p "Hey " + person_1 + " and " + person_2
+  end 
+end 
+
+greet("Chao") # => "Hey Chao"
+greet("Chao", "Arittro") # => "Hey Chao and Arittro"
+
+# It's best practice to have optional parameters listed after
+# the required ones.
