e292bced5922542d0ef358f0d4662f14f80501ce
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-27 23:24:04 +0000

    option hashes notes updated

--- software_engineering_foundations/w1d1/option_hashes.rb
+++ software_engineering_foundations/w1d1/option_hashes.rb
@@ -8,3 +8,15 @@ method({"location"=>"SF", "color"=>"red", "size"=>100})
 
 # this also works
 method("location"=>"SF", "color"=>"red", "size"=>100)
+
+# This can really clean things up when you have other arguments
+# before the hash
+def modify_string(str, options)
+  str.upcase! if options["upper"]
+  p str * options["repeats"]
+end 
+
+# less readable
+modify_string("bye", {"upper"=>true, "repeats"=>3}) # => "BYEBYEBYE"
+# more readable
+modify_string("bye", "upper"=>true, "repeats"=>3) # => "BYEBYEBYE"
