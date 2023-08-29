9fe1cc5fd23d16374f98b957a79cadfe233124e9
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-29 19:50:12 +0000

    modified splat operator file, added advanced exercise files and rails info

--- software_engineering_foundations/w1d1/splat_operator.rb
+++ software_engineering_foundations/w1d1/splat_operator.rb
@@ -37,3 +37,29 @@ end
 method("a", "b", "c")
 
 method("a", "b")
+
+# Using splat to decompose an array
+def greet(first_name, last_name)
+  p "Hey " + first_name + ", your last name is " + last_name
+end 
+
+names = ["grace", "hopper"]
+greet(names) # ArgumentError: wrong number of arguments (given 1, expected 2)
+
+def greet(first_name, last_name)
+  p "Hey " + first_name + ", your last name is " + last_name
+end 
+
+names = ["grace", "hopper"]
+greet(*names) # = > "Hey Grace, your last name is Hopper"
+
+arr_1 = ["a", "b"]
+arr_2 = ["d", "e"]
+arr_3 = [ *arr_1, "c", *arr_2 ]
+p arr_3 # => ["a", "b", "c", "d", "e"]
+
+# Using splat to decompose a hash
+# Using double splat (**)
+old_hash = { a: 1, b: 2 }
+new_hash = { **old_hash, c: 3 }
+p new_hash # => {:a =>1, :b=>2, :c=>3 }
