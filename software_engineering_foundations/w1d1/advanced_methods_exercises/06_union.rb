5760a3346e2fdb075c7b50b5563ab4006b3f10ad
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-30 14:59:35 +0000

    updated union exercise, used inject instead of flatten

--- software_engineering_foundations/w1d1/advanced_methods_exercises/06_union.rb
+++ software_engineering_foundations/w1d1/advanced_methods_exercises/06_union.rb
@@ -1,7 +1,7 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
 def union(*arr) 
-  arr.flatten
+  arr.inject { |acc, array| acc + array }
 end
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
